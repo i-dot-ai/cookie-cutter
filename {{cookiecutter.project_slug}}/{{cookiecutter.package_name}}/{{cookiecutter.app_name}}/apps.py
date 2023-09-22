30cb1d1c4d25077ae5ada284374df4b9fe10d045
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-09-22 11:51:16 +0100

    Added comment to apps.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/{{cookiecutter.app_name}}/apps.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/{{cookiecutter.app_name}}/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class {{cookiecutter.app_name.capitalize()}}Config(AppConfig):
+class {{cookiecutter.app_name.capitalize()}}Config(AppConfig):  # TODO: It's likely you'll have to fix this class name
     default_auto_field = "django.db.models.BigAutoField"
     name = "{{cookiecutter.package_name}}.{{cookiecutter.app_name}}"
