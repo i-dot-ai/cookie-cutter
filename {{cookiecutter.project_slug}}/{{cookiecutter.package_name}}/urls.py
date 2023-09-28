525b1a53b485405f1618a819fc2cd70c580e464c
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-09-28 10:59:38 +0100

    Added info_views to urls.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/urls.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from {{cookiecutter.package_name}}.{{cookiecutter.app_name}} import views
+from {{cookiecutter.package_name}}.{{cookiecutter.app_name}} import views, info_views
 
-urlpatterns = [
+info_urlpatterns = [
+    path("privacy-notice/", info_views.privacy_notice_view, name="privacy-notice"),
+    path("accessibility-statement/", info_views.accessibility_statement_view, name="accessibility-statement"),
+    path("support/", info_views.support_view, name="support"),
+]
+
+other_urlpatterns = [
     path("", views.index_view, name="index"),
     path("home/", views.homepage_view, name="homepage"),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
 ]
+
+urlpatterns = info_urlpatterns + other_urlpatterns
